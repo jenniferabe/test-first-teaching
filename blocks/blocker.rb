9c8e9f3e5f7d528d6fc85a17302d15ff595bb21c
Jennifer Abe <jenniferabe@gmail.com>
2009-09-07 19:08:43 -0700

    adding my blocker implementation from classto new clone

--- blocks/blocker.rb
+++ blocks/blocker.rb
@@ -1,2 +1,14 @@
 class Blocker
+  def execute(count=1)
+    result = nil
+    count.times do
+      result = yield
+    end
+    result
+  end
+  def self.add_one
+    yield + 1
+  end
+end
+class Blocker
 end
